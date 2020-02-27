beee76d43b5bbc06f0628ab660d02bf4bef39720
Kevin Hubert <34512569+KevinHubert-Dev@users.noreply.github.com>
2020-02-27 10:56:02 -0800

    docu: Added comments for waypoint_updater.py which are used for additional explaination about the executed code

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -30,12 +30,14 @@ class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
+        # Subscribe to ROS-topics to get current-position and waypoint information
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
+        # Create ROS-topic-publisher to pass the final_waypoints to drive to other nodes.
+        # queue_size=1 to ensure non old informations is used by subscribers.
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
@@ -46,48 +48,66 @@ class WaypointUpdater(object):
 
         self.loop()
 
+    # Setup rospy-utils to keep the publishing at a specific frequency (in ours case 50x per seconds (20ms))
     def loop(self):
         rate = rospy.Rate(50)
+        # Keep running until the node gets shutdown-signal
         while not rospy.is_shutdown():
+            # Avoid race-conditions when base_waypoints or current position was not intialized
             if self.pose and self.base_waypoints:
-                # Get closest waypoint
+                # Get closest waypoint to drive to
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
                 self.publish_waypoints(closest_waypoint_idx)
+            # Use ros-sleep-function to reach the required frequency
             rate.sleep()
 
+    # Use hyperplane and current-car-position to get the closest waypoint to drive to
     def get_closest_waypoint_idx(self):
+        # Get current car position on x-y-axis
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
+        # Search for closest waypoint in KD-tree: https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.KDTree.query.html
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
 
-        # Check if closest is ahead or behind vehicle
+        # Get the closest and previous coord to check if the closest coords in infront or behind us
         closest_coord = self.waypoints_2d[closest_idx]
         prev_coord = self.waypoints_2d[closest_idx-1]
 
         # Equation for hyperplane through closest_coords
-        cl_vect = np.array(closest_coord)
-        prev_vector = np.array(prev_coord)
-        pos_vect = np.array([x, y])
+        cl_vect = np.array(closest_coord)  # closest waypoint
+        prev_vector = np.array(prev_coord) # waypoint befoe closest
+        pos_vect = np.array([x, y])        # our position
 
+        # Use hyperplane to find out if the given waypoint in infront or behind us which indicates if 
+        # we should consider the waypoint in our path-planning or use the next waypoint. Visualization:
+        # https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Hesse_normalenform.svg/1200px-Hesse_normalenform.svg.png
         dot_product = np.dot(cl_vect-prev_vector, pos_vect-cl_vect)
 
-        # If closest waypoint is behind vehicle
+        # If closest waypoint is behind vehicle use waypoint beyond the closest
         if dot_product > 0:
+            # Use modulo operator to avoid out-of-range (restart at begin if end is reached)
             closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
         
         return closest_idx
 
+    # Send the waypoints to the subscribed notes
     def publish_waypoints(self, closest_idx):
-        lane = Lane()
-        lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        lane = Lane() # ROS Lane instance
+        lane.header = self.base_waypoints.header # Reuse current header-information from basic waypoints
+        # Add closest waypoint which is infront of us +LOOKAHEAD_WPS waypoint beyond 
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS] 
         self.final_waypoints_pub.publish(lane)
 
+    # Refresh current car-position information
     def pose_cb(self, msg):
         self.pose = msg
 
+    # Base-Waypoints are just published once. Ensure to store them in our variables
     def waypoints_cb(self, waypoints):
+        # Store waypoints in local variable for class-instance
         self.base_waypoints = waypoints
+        # Create the KDTree which help us to find the waypoint-informations way more efficient
+        # KDTree well explained can be found here: https://www.youtube.com/watch?v=TLxWtXEbtFE
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
